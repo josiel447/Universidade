a12444e14a5a8cadecf552ecbd429cddd7f5fa48
josiel447 <josieldossantos447@gmail.com>
2026-03-04 21:46:15 -0300

    1.2

---
+++ Requisitos/Class/Aluno.java
@@ -0,0 +1,16 @@
+public class Aluno{
+
+    private String matricula;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String endereco;
+    public String getMatricula(){
+        return matricula;
+    }
+    public void setMatricula(String matricula){
+
+        this.matricula = matricula;
+    }
+
+}
---
+++ Requisitos/Class/Proferssor.java
@@ -0,0 +1,13 @@
+public class Proferssor{
+     private String matricula;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String endereco;
+    public String getMatricula(){
+        return matricula;
+    }
+    public void setMatricula(String matricula){
+        this.matricula = matricula ;
+    }
+}
